dcea317bdbeee4e66ecc9aa6a7a769bec95f4151
bengquist <engquist_blake@yahoo.com>
2019-03-08 18:45:48 -0600

    add item to resolvers

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,16 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if logged in
+
+    return await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+  }
+};
 
 module.exports = Mutations;
